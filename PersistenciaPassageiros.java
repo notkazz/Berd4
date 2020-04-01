f75815e338539b60ef0ab896a974b11fc9d82652
Thales Santin <kazztt@hotmail.com>
2020-04-01 04:29:10 -0300

    PersistenciaPassageiros edited

--- PersistenciaPassageiros.java
+++ PersistenciaPassageiros.java
@@ -1,8 +1,10 @@
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
@@ -35,10 +37,17 @@ public class PersistenciaPassageiros {
         }
     }
 
-    public void persisteMotoristas(LinkedList<Motoristas> list) throws IOException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH)); CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);){
-            
-            
-        }
+
+    public void persistePassageiros(LinkedList<Passageiros> list) throws IOException {
+            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH)); CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("cpf", "nome", "cartao", "formaPGTO"));){
+                LinkedList<Passageiros> listOriginal= carregaPassageiros(); 
+                if(list.size()>listOriginal.size()){
+                    for(int i= 0; i<=list.size(); i++){
+                        Passageiros helper= list.get(i);
+                        csvPrinter.printRecord(helper.getCPF(), helper.getCartao(), helper.getNome(), helper.getFormaPGTO());
+                    }
+                    csvPrinter.flush();
+                }
     } 
+}
 }
